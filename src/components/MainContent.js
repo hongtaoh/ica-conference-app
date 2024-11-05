7ef2d71a84a5c4c051e1b1326acc85544a3fb2d8
hongtaoh <hongtaoh@cs.wisc.edu>
2024-11-05 14:09:45 -0600

    adjust header width on phones.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import Header from './Header';
 
 const MainContent = ({ children }) => (
-  <>
+  <Container maxWidth="xl" sx={{ mt: { xs: 6, md: 14 }, px: { xs: 2, sm: 4, md: 0 } }}>
     <Header />
     <Box sx={{ mt: { xs: 9, sm: 10 }, maxWidth: 'xl', mx: 'auto', px: 2 }}>
       {children}
     </Box>
-  </>
+    </Container>
 );
 
 export default MainContent;
